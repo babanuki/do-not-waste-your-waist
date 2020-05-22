c11a77a6e81f031fb561fc7f0b8175a2516b8ef3
babanuki <52341660+babanuki@users.noreply.github.com>
2020-05-23 08:08:56 +0900

    Update web.js

--- code/web.js
+++ code/web.js
@@ -8,7 +8,9 @@ mysql = require('mysql');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-var user_id=0;
+app.use(express.static(__dirname));
+
+var user_id=undefined;
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -19,20 +21,24 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-app.get('/', function (req, res) {
-/*    if(user_id==undefined){
-	var login_html=fs.readFile('./login.html', function(err, login_html){
-	    user_id=window.document.getElementById('user').value;	
-	});
-	console.log(user_id);
-    }
-*/
+app.get('/', function(req, res){
+    var html=fs.readFile('./login.html', function(err, html){
+	html=""+html;
+	    res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.end();
+    });
+});
+
+app.get('/id', function (req, res) {
+    if(req.query.id!=undefined)
+    	user_id=req.query.id;
     console.log('got app.get(graph)');
     var html = fs.readFile('./graph.html', function (err, html) {
     html = ""+ html
     console.log('read file');
     
-    var qstr = 'select * from degree where time>SUBDATE(NOW(), INTERVAL 1 minute) AND user_id='+user_id;
+    var qstr = 'select * from degree where user_id='+user_id;
     connection.query(qstr, function(err, rows, cols) {
       if (err) throw err;
       var pointX;
@@ -40,18 +46,20 @@ app.get('/', function (req, res) {
       var point=""
       var data = "";
       var comma = ""
-      for (var i=0; i< rows.length; i++) {
+      for (var i=rows.length-1; i>-1; i--) {
+	 if(i==rows.length-3600)
+	      break;
+
          r = rows[i];
-         data += comma + "[new Date("+moment(r.time).add('-1', 'months').format('YYYY,MM,DD,HH,mm,ss')+"),"+r.x_angle+","+r.y_angle+"]";
+         data += comma + "["+i+","+r.x_angle+","+r.y_angle+"]";
          comma = ",";
 
 	 if(i==rows.length-1)
 	      pointX=r.x_angle, pointY=r.y_angle;
       }
+      point="X="+pointX+"       |       Y="+pointY;
 
-      point="X="+pointX+"   |   Y="+pointY;
-
-      var header ="data.addColumn('datetime', 'Date/Time');"
+      var header ="data.addColumn('number', 'Time');"
       header += "data.addColumn('number', 'X_ANGLE');"
       header += "data.addColumn('number', 'Y_ANGLE');"
             console.log(header);
@@ -65,11 +73,11 @@ app.get('/', function (req, res) {
       var color = "";
       var comment="";
 
-      if(pointX>-15 && pointX<15 && pointY>80){
+      if(pointX>-15 && pointX<15 && pointY>-10 && pointY<10){
 	    color+="#00CC00";
 	    comment+="Ohhh, your waist is good enough!<br>Keep that LUL\n";
       }
-      else if(pointX>-24 && pointX<24 && pointY>70){
+      else if(pointX>-24 && pointX<24 && pointY>-20 && pointY<20){
 	    color+="#CCCC00";
 	    comment+="Nah, your waist may be in trouble...\n";
       }
@@ -87,6 +95,6 @@ app.get('/', function (req, res) {
   });
 })
 
-var server = app.listen(8082, function () {
+var server = app.listen(8081, function () {
   console.log('Web is Running...')
 });
